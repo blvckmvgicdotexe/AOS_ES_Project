3dc37c96db270957f237f776c05135d4e9292f3c
Federico Bocchieri <federico.bocchieri@mail.polimi.it>
2026-07-27 18:33:48 +0200

    Added final class + modified main to finish project

--- PMDB_IR.hpp
+++ PMDB_IR.hpp
@@ -14,3 +14,230 @@ struct Sample {
 };
 
 typedef vector<Sample> Samples;
+
+
+class PMDB_IR {
+
+    private:
+        // Type definitions for used pins
+        typedef Gpio<PB, 10> IR_LED;
+        typedef Gpio<PA, 10> IR_Receiver;
+
+        Samples buffer;                     // Buffer for recorded samples
+        const int NUM_SAMPLES = 512;
+
+        Thread * is_recording = nullptr;    // Condition variable to wait for conversion to be complete
+
+    public:
+
+        // Setters / getters
+        uint info_rec_len_ms() {
+            return buffer.at(buffer.size() - 1).timestamp_us / 1000;
+        }
+
+        uint info_rec_num_samples() {
+            return buffer.size();
+        }
+
+        PMDB_IR() {
+            buffer.reserve(NUM_SAMPLES);    // Preallocate space for samples for efficient insertion
+
+            // Configure required pins
+            // Notably, TIM2 is reconfigured each time when needed
+            {
+                GlobalIrqLock lock;
+
+                // Configure IR receiver pin with interrupt on edge change
+                IR_Receiver::mode(Mode::INPUT); // Configure IR receiver pin as input
+                SYSCFG->EXTICR[2] = SYSCFG_EXTICR3_EXTI10_PA;   // Route IR pin (PA10) to EXTI10 line
+                EXTI->RTSR |= EXTI_RTSR_TR10;   // Trigger interrupt on rising edge
+                EXTI->FTSR |= EXTI_FTSR_TR10;   // Trigger interrupt on falling edge
+                IRQregisterIrq(lock, EXTI15_10_IRQn, &PMDB_IR::isr_sample, this);   // Register ISR to sample on each signal edge
+
+                // Configure IR LED pin to be driven by TIM2 CH3 in PWM mode
+                IR_LED::mode(Mode::ALTERNATE);
+                IR_LED::alternateFunction(1);   // TIM2 CH3 (PWM) output
+            }
+        }
+
+        ~PMDB_IR() {
+            GlobalIrqLock lock;
+            // Disable interrupts on GPIO
+            EXTI->IMR &= ~EXTI_IMR_IM10;
+            EXTI->RTSR &= ~EXTI_RTSR_TR10;
+            EXTI->FTSR &= ~EXTI_FTSR_TR10;
+            IRQunregisterIrq(lock, EXTI15_10_IRQn, &PMDB_IR::isr_sample, this);   // Unregister ISR
+        }
+
+        /*
+        * Clear any interrupts on TIM2 and reset timer before repurposing it
+        */
+        inline void reset_TIM2() {
+            GlobalIrqLock lock;
+
+            // Put TIM2 in reset state via RCC
+            RCC->APB1RSTR |= RCC_APB1RSTR_TIM2RST;
+            RCC->APB1RSTR &= ~RCC_APB1RSTR_TIM2RST;
+
+            // Unregister ISR on TIM2
+            if (IRQisIrqRegistered(TIM2_IRQn)) {
+                IRQunregisterIrq(lock, TIM2_IRQn, &PMDB_IR::isr_stop_sampling, this);
+            }
+
+        }
+
+        /*
+        * Configure TIM2 for sampling (up counter one-pulse mode, 1us resolution, duration of 1s i.e. overflow after 1s with interrupt on update)
+        */
+        void configure_TIM2_sampler() {
+            GlobalIrqLock lock;
+            reset_TIM2();
+
+            RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;     // Enable timer clock
+            TIM2->CR1 |= TIM_CR1_OPM;               // One-pulse mode (stops timer at the next update event e.g. an overflow or setting the UG bit)
+            TIM2->PSC = 84000000 / 1000000 - 1;     // Configure 1MHz timer (i.e. 1us resolution).
+            TIM2->ARR = 1000000 - 1;                // Count 1000000us == 1s
+            TIM2->EGR |= TIM_EGR_UG;                // Generate update event to update register values
+            TIM2->SR &= ~TIM_SR_UIF;                // Clear update flag caused by setting UG
+            IRQregisterIrq(lock, TIM2_IRQn,
+                &PMDB_IR::isr_stop_sampling, this); // Register ISR to stop sampling
+        }
+
+        /*
+        * Configure TIM2 CH3 to drive IR LED (38KHz 50% DC square wave) to stimulate IR receiver
+        */
+        void configure_TIM2_emitter() {
+            FastGlobalIrqLock lock;
+            reset_TIM2();
+
+            RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;     // Enable clock to TIM2
+            TIM2->CR1 |= TIM_CR1_ARPE;              // Enable auto-reload preload for timer
+            TIM2->CCMR2 |= TIM_CCMR2_OC3M_2
+                | TIM_CCMR2_OC3M_1;                 // Enable PWM mode 1 (110) for channel 3
+            TIM2->CCMR2 |= TIM_CCMR2_OC3PE;         // Enable auto-reload preload for channel 3
+            TIM2->PSC = 0;                          // No prescaling, keep time base at 84MHz
+            TIM2->ARR = 84000 / 38 - 1;             // Set frequency to 38KHz
+            TIM2->CCR3 = (84000 / 38 - 1) / 2;      // Set DC to 50%
+            TIM2->CNT=0;                            // Reset counter to 0
+            TIM2->EGR |= TIM_EGR_UG;                // Reinitialize counter and update registers
+            TIM2->CR1 |= TIM_CR1_CEN;               // Start time base (notably, the timer keeps running forever from this point on)
+            TIM2->CCER &= ~TIM_CCER_CC3E;           // Make sure channel output is disabled for now
+        }
+
+
+        /*
+        * Send the last sampled IR data by toggling TIM2 channel 3 (PWM) on and off
+        * This is done in polling and with disabled interrupts to ensure precision;
+        * this is probably not a problem since a recording lasts tens of milliseconds.
+        */
+        void send() {
+            configure_TIM2_emitter();       // Reconfigure TIM2 as a 38KHz 50% DC square wave to stimulate receiver
+            {
+                FastGlobalIrqLock irqLock;
+
+                uint start_ts_us = IRQgetTime() / 1000; // Record initial timestamp
+
+                for (auto& sample: buffer) {
+
+                    int next_ts_us = start_ts_us + sample.timestamp_us;
+                    while (IRQgetTime() / 1000 - next_ts_us < 0) ;  // Busy wait until system timer exceeds
+                                                                    // the current sample's timestamp (+ initial timestamp)
+
+                    // Toggle PWM channel according to signal edge
+                    if (sample.level)                   // Rising edge
+                        TIM2->CCER &= ~TIM_CCER_CC3E;   // Disable timer channel
+                    else                                // Falling edge -> stimulate receiver (active low)
+                        TIM2->CCER |= TIM_CCER_CC3E;    // Enable timer channel
+                }
+
+                // Disable timer channel for good measure
+                TIM2->CCER &= ~TIM_CCER_CC3E;
+            }
+        }
+
+        /*
+        * Capture IR data by sampling the IR receiver's pin on signal edge
+        * Capture starts on the first signal edge and stops after 1 second and is done with interrupts
+        * - The first interrupt is triggered on each signal edge to sample another point
+        * - The second interrupt is triggered when the 1 second timer elapses
+        *  to disable both interrupts and essentially stop the recording
+        * On function return, signal is memorised and ready to be used by 'send()'.
+        */
+        void receive() {
+            buffer.clear();             // Empty buffer before receiving
+            configure_TIM2_sampler();   // Reconfigure TIM2 as a one-shot 1s timer with 1us resolution
+            {
+                FastGlobalIrqLock lock;
+                TIM2->DIER |= TIM_DIER_UIE; // Enable "stop_sampling" ISR on timer update (overflow)
+                EXTI->IMR |= EXTI_IMR_IM10; // Enable "sample_signal" ISR on GPIO level change
+
+                is_recording = Thread::IRQgetCurrentThread();
+                do {
+                    Thread::IRQglobalIrqUnlockAndWait(lock);
+                } while (is_recording);  // Spin on condition variable
+            }
+        }
+
+        /*
+        * Pretty print recorded signal as a VCD (value change dump) file
+        * that can be copypasted into a .vcd file and opened in PulseView / GTKWave
+        */
+        void pprint_vcd() {
+            const uint shift_us = 10000;
+            iprintf("$version PMDB16 IR dumper $end\n");
+            iprintf("$timescale 1 us $end\n");
+            iprintf("$scope module top $end\n");
+            iprintf("$var wire 1 ! ir $end\n");
+            iprintf("$upscope $end\n");
+            iprintf("$enddefinitions $end\n");
+            iprintf("#0 1!\n");                         // "fake" rising edge
+            for (auto& sample: buffer) {
+                iprintf("#%d %d!\n", sample.timestamp_us + shift_us, sample.level);
+            }
+        }
+
+    private:
+
+        // --- Interrupts ---
+
+        /*
+        * This interrupt starts the timer and samples the signal level of the GPIO
+        * together with its timestamp, then wakes up the caller thread to log info
+        */
+        void isr_sample() {
+
+            // Check that interrupt is pending for EXTI10
+            if (!(EXTI->PR & EXTI_PR_PR10)) {
+                return;
+            }
+            EXTI->PR |= EXTI_PR_PR10;   // Clear interrupt pending bit
+
+            // Start timer if not started already
+            if (!(TIM2->CR1 & TIM_CR1_CEN)) {   // Note: in one-pulse mode, CEN bit is cleared automatically when timer elapses
+                TIM2->CR1 |= TIM_CR1_CEN;       // Start timer
+            }
+
+            // Sample timestamp and IR port level
+            bool signal_level = IR_Receiver::value();
+            uint timestamp_us = TIM2->CNT;
+            buffer.push_back({signal_level, timestamp_us}); // Note: interrupt is triggered on signal edge, thus level == 1 => rising edge
+        }
+
+        /*
+        * This interrupt disables all interrupts to stop recording
+        */
+        void isr_stop_sampling() {
+
+            // Disable all interrupts to stop recording, then wake up the receiver function
+            if (TIM2->SR & TIM_SR_UIF) {
+                TIM2->SR &= ~TIM_SR_UIF;       // Clear update flag
+                EXTI->IMR &= ~EXTI_IMR_IM10;    // Disable edge triggered interrupt on GPIO pin
+                TIM2->DIER &= ~TIM_DIER_UIE;   // Disable this interrupt (timer update pin on overflow)
+
+                // Wake up receiver function
+                is_recording->IRQwakeup();   // Will occur as soon as going out of the interrupt
+                is_recording = nullptr;
+            }
+        }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include <thread>
 #include <vector>
 #include "IREmitter.hpp"
 #include "IRReader.hpp"
+#include "PMDB_IR.hpp"
 #include "drivers/gpio/stm32_gpio.h"
 #include "kernel/lock.h"
 #include "kernel/thread.h"
@@ -11,6 +13,37 @@
 
 using namespace std;
 using namespace miosix;
+
+
+void cls() {
+    iprintf("\e[1;1H\e[2J");
+}
+
+void print_header() {
+    iprintf("\n");
+    iprintf("  ██╗██████╗     ███████╗ █████╗ ███╗   ███╗██████╗ ██╗     ███████╗██████╗ \n");
+    iprintf("  ██║██╔══██╗    ██╔════╝██╔══██╗████╗ ████║██╔══██╗██║     ██╔════╝██╔══██╗\n");
+    iprintf("  ██║██████╔╝    ███████╗███████║██╔████╔██║██████╔╝██║     █████╗  ██████╔╝\n");
+    iprintf("  ██║██╔══██╗    ╚════██║██╔══██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔══██╗\n");
+    iprintf("  ██║██║  ██║    ███████║██║  ██║██║ ╚═╝ ██║██║     ███████╗███████╗██║  ██║\n");
+    iprintf("  ╚═╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝\n");
+    iprintf("  by Federico Bocchieri\n");
+
+}
+
+void print_send(uint duration, uint num_samples) {
+    iprintf("\n");
+    iprintf("\t[ SEND MODE ]\tPress once to send\tLong press to switch mode\n\n");
+    iprintf("\t\tLast sampled signal lasts %d ms with %d samples\n", duration, num_samples);
+    iprintf("\n");
+}
+
+void print_receive() {
+    iprintf("\n");
+    iprintf("\t[ RECV MODE ]\tPress once to receive\tLong press to switch mode\n");
+    iprintf("\n");
+
+}
 
 /*
  * Plan: generate a PWM signal at 38KHz (50% DC) using TIM2 CH3 and route it towards pin PB10 connected to the IR LED
@@ -24,171 +57,100 @@ using namespace miosix;
  */
 int main()
 {
-    iprintf("IR Remote emitter\n");
 
-    Wave wave = {{RISING_EDGE, 0},
-        {FALLING_EDGE, 19991},
-        {RISING_EDGE, 20352},
-        {FALLING_EDGE, 21004},
-        {RISING_EDGE, 21360},
-        {FALLING_EDGE, 22016},
-        {RISING_EDGE, 22372},
-        {FALLING_EDGE, 24058},
-        {RISING_EDGE, 24414},
-        {FALLING_EDGE, 25070},
-        {RISING_EDGE, 25426},
-        {FALLING_EDGE, 26082},
-        {RISING_EDGE, 26438},
-        {FALLING_EDGE, 27094},
-        {RISING_EDGE, 27451},
-        {FALLING_EDGE, 28106},
-        {RISING_EDGE, 28463},
-        {FALLING_EDGE, 30148},
-        {RISING_EDGE, 30505},
-        {FALLING_EDGE, 31160},
-        {RISING_EDGE, 31517},
-        {FALLING_EDGE, 33198},
-        {RISING_EDGE, 33554},
-        {FALLING_EDGE, 34210},
-        {RISING_EDGE, 34566},
-        {FALLING_EDGE, 35226},
-        {RISING_EDGE, 35583},
-        {FALLING_EDGE, 36239},
-        {RISING_EDGE, 36595},
-        {FALLING_EDGE, 38276},
-        {RISING_EDGE, 38632},
-        {FALLING_EDGE, 39288},
-        {RISING_EDGE, 39644},
-        {FALLING_EDGE, 87569},
-        {RISING_EDGE, 87926},
-        {FALLING_EDGE, 88581},
-        {RISING_EDGE, 88938},
-        {FALLING_EDGE, 89593},
-        {RISING_EDGE, 89954},
-        {FALLING_EDGE, 91635},
-        {RISING_EDGE, 91992},
-        {FALLING_EDGE, 92647},
-        {RISING_EDGE, 93004},
-        {FALLING_EDGE, 93659},
-        {RISING_EDGE, 94020},
-        {FALLING_EDGE, 95701},
-        {RISING_EDGE, 96058},
-        {FALLING_EDGE, 97739},
-        {RISING_EDGE, 98095},
-        {FALLING_EDGE, 98751},
-        {RISING_EDGE, 99112},
-        {FALLING_EDGE, 100792},
-        {RISING_EDGE, 101149},
-        {FALLING_EDGE, 101805},
-        {RISING_EDGE, 102161},
-        {FALLING_EDGE, 103842},
-        {RISING_EDGE, 104199},
-        {FALLING_EDGE, 105879},
-        {RISING_EDGE, 106240},
-        {FALLING_EDGE, 107921},
-        {RISING_EDGE, 108277},
-        {FALLING_EDGE, 108933},
-        {RISING_EDGE, 109289},
-        {FALLING_EDGE, 110970},
-        {RISING_EDGE, 111331},
-        {FALLING_EDGE, 155148},
-        {RISING_EDGE, 155504},
-        {FALLING_EDGE, 156160},
-        {RISING_EDGE, 156516},
-        {FALLING_EDGE, 157172},
-        {RISING_EDGE, 157533},
-        {FALLING_EDGE, 159213},
-        {RISING_EDGE, 159570},
-        {FALLING_EDGE, 160225},
-        {RISING_EDGE, 160582},
-        {FALLING_EDGE, 161238},
-        {RISING_EDGE, 161598},
-        {FALLING_EDGE, 162250},
-        {RISING_EDGE, 162610},
-        {FALLING_EDGE, 163266},
-        {RISING_EDGE, 163623},
-        {FALLING_EDGE, 165303},
-        {RISING_EDGE, 165660},
-        {FALLING_EDGE, 166315},
-        {RISING_EDGE, 166676},
-        {FALLING_EDGE, 168357},
-        {RISING_EDGE, 168713},
-        {FALLING_EDGE, 169369},
-        {RISING_EDGE, 169726},
-        {FALLING_EDGE, 170381},
-        {RISING_EDGE, 170738},
-        {FALLING_EDGE, 171393},
-        {RISING_EDGE, 171754},
-        {FALLING_EDGE, 173435},
-        {RISING_EDGE, 173791},
-        {FALLING_EDGE, 174447},
-        {RISING_EDGE, 174803},
-        {FALLING_EDGE, 222725},
-        {RISING_EDGE, 223086},
-        {FALLING_EDGE, 223738},
-        {RISING_EDGE, 224098},
-        {FALLING_EDGE, 224754},
-        {RISING_EDGE, 225110},
-        {FALLING_EDGE, 226791},
-        {RISING_EDGE, 227152},
-        {FALLING_EDGE, 227803},
-        {RISING_EDGE, 228164},
-        {FALLING_EDGE, 228816},
-        {RISING_EDGE, 229176},
-        {FALLING_EDGE, 230857},
-        {RISING_EDGE, 231214},
-        {FALLING_EDGE, 232895},
-        {RISING_EDGE, 233255},
-        {FALLING_EDGE, 233907},
-        {RISING_EDGE, 234268},
-        {FALLING_EDGE, 235948},
-        {RISING_EDGE, 236305},
-        {FALLING_EDGE, 236961},
-        {RISING_EDGE, 237321},
-        {FALLING_EDGE, 238998},
-        {RISING_EDGE, 239359},
-        {FALLING_EDGE, 241040},
-        {RISING_EDGE, 241396},
-        {FALLING_EDGE, 243077},
-        {RISING_EDGE, 243437},
-        {FALLING_EDGE, 244089},
-        {RISING_EDGE, 244450},
-        {FALLING_EDGE, 246130},
-        {RISING_EDGE, 246487}};
 
-    using user_btn_PC13 = Gpio<PC, 13>;
+    using blue_button = Gpio<PC, 13>;
 
-    {   // Device configuration
-        FastGlobalIrqLock lock; // RAII to disable IRQs in this scoped block (critical section)
-
-        // Input button configuration
-        user_btn_PC13::mode(Mode::INPUT);
+    // Input button configuration
+    {
+        FastGlobalIrqLock lock;
+        blue_button::mode(Mode::INPUT);
     }
 
-    //IREmitter ir;
-    IRReader ir_reader;
+    // Definitions for button keypress
+    enum keystate {PRESSED = 0, NOT_PRESSED = 1} prev_keystate, curr_keystate = NOT_PRESSED;
 
-    enum keystate {PRESSED = 0, NOT_PRESSED = 1};
-    keystate prev_keystate, curr_keystate = NOT_PRESSED;
+    uint longpress_threshold_ms = 0;
+    const uint MODE_LONGPRESS_MS = 1000; // Wait for 1s before switching mode
 
+    // Definitions for toggling mode
+    enum mode { IR_RECEIVE, IR_SEND } mode = IR_RECEIVE;
+    static bool long_press = false;
+
+    PMDB_IR ir;
+
+    // Infinite loop to read blue button keypress
     while(true) {
 
-        // Poll continuously for keypress
+        cls();
+        print_header();
+        switch (mode) {
+            case IR_RECEIVE:
+            print_receive();
+            break;
+
+            case IR_SEND:
+            print_send(ir.info_rec_len_ms(), ir.info_rec_num_samples());
+        }
+
+        // Poll for key press
         do {
+            // Remember last key state
             prev_keystate = curr_keystate;
-            curr_keystate = static_cast<keystate>(user_btn_PC13::value());
+            curr_keystate = static_cast<keystate>(blue_button::value());
+
+            // Look for a "rising edge"
             if (!(prev_keystate == NOT_PRESSED && curr_keystate == PRESSED)) {
                 this_thread::sleep_for(10ms);
-            } else break;
+            } else {
+                longpress_threshold_ms = getTime() / 1000000 + MODE_LONGPRESS_MS;
+                break;
+            };
         } while (true);
 
-        iprintf("Capturing wave\n");
+        // Poll for key release
+        do {
+            // Likewise
+            prev_keystate = curr_keystate;
+            curr_keystate = static_cast<keystate>(blue_button::value());
 
-        ir_reader.receive();
+            if (!(prev_keystate == PRESSED && curr_keystate == NOT_PRESSED)) {  // Button still pressed
+                // If time exceeds threshold, signal keypress and stop looking for "falling edges"
+                uint curr_ts_ms = getTime() / 1000000;
+                if (curr_ts_ms >= longpress_threshold_ms) {
+                    long_press = true;
+                    break;
+                } else {
+                    this_thread::sleep_for(10ms);
+                }
+            } else {    // Button released
+                break;
+            };
+        } while (true);
 
-        // ir.send(wave);
+        // Toggle IR mode in case of a long press and start reading button again
+        if (long_press) {
+            if (mode == IR_SEND)
+                mode = IR_RECEIVE;
+            else
+                mode = IR_SEND;
+            long_press = !long_press;
+            iprintf("Switching to %s mode\n", mode == IR_SEND ? "send" : "receive");
+            continue;
+        }
 
-        // iprintf("Wave sent.\n");
+        // Act upon current mode
 
+        switch (mode) {
+            case IR_RECEIVE:
+            iprintf("\t\t\t\tReceiving IR...\n");
+            ir.receive();
+            mode = IR_SEND;
+            break;
+
+            case IR_SEND:
+            iprintf("\t\t\t\tSending IR...\n");
+            ir.send();
+        }
     }
 }
